1abd57fa008abe11502f4e9904829491b4a4f48b
kousarfathima <128119973+kousarfathima@users.noreply.github.com>
2023-06-12 17:41:10 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int rows = 2;
+        int count = 1;
+
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= count; j++)
+            {
+                for (int k = 1; k <= j; k++)
+                {
+                    Console.Write(count + " ");
+                }
+            }
+            count++;
+        }
+
+        Console.WriteLine();
+    }
+}
